0fadaff8f0f70583bbf35841c015ef608e725044
saqibjaved2906 <saqibjaved2906@gmail.com>
2016-10-24 21:18:05 +0500

    First commit

---
+++ Documents/Visual Studio 2012/Projects/TicTacToe_GUI/TicTacToe_GUI/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace TicTacToe_GUI
+{
+    partial class gamename
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.a1 = new System.Windows.Forms.Button();
+            this.a2 = new System.Windows.Forms.Button();
+            this.a3 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.c1 = new System.Windows.Forms.Button();
+            this.c2 = new System.Windows.Forms.Button();
+            this.c3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gamename2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.powins = new System.Windows.Forms.Label();
+            this.pxwins = new System.Windows.Forms.Label();
+            this.restart = new System.Windows.Forms.Button();
+            this.gamewinner = new System.Windows.Forms.Label();
+            this.win = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // a1
+            // 
+            this.a1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.a1.Font = new System.Drawing.Font("Segoe Script", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a1.Location = new System.Drawing.Point(0, 62);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(70, 70);
+            this.a1.TabIndex = 0;
+            this.a1.Text = "A1";
+            this.a1.UseVisualStyleBackColor = false;
+            this.a1.Click += new System.EventHandler(this.a1_Click);
+            // 
+            // a2
+            // 
+            this.a2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.a2.Font = new System.Drawing.Font("Segoe Script", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a2.Location = new System.Drawing.Point(76, 62);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(70, 70);
+            this.a2.TabIndex = 1;
+            this.a2.Text = "A2";
+            this.a2.UseVisualStyleBackColor = false;
+            this.a2.Click += new System.EventHandler(this.a2_Click);
+            // 
+            // a3
+            // 
+            this.a3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.a3.Font = new System.Drawing.Font("Segoe Script", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a3.Location = new System.Drawing.Point(152, 62);
+            this.a3.Name = "a3";
+            this.a3.Size = new System.Drawing.Size(70, 70);
+            this.a3.TabIndex = 2;
+            this.a3.Text = "A3";
+            this.a3.UseVisualStyleBackColor = false;
+            this.a3.Click += new System.EventHandler(this.a3_Click);
+            // 
+            // b1
+            // 
+            this.b1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.b1.Font = new System.Drawing.Font("Segoe Script", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.Location = new System.Drawing.Point(0, 138);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(70, 70);
+            this.b1.TabIndex = 3;
+            this.b1.Text = "B1";
+            this.b1.UseVisualStyleBackColor = false;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b2
+            // 
+            this.b2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.b2.Font = new System.Drawing.Font("Segoe Script", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b2.Location = new System.Drawing.Point(76, 138);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(70, 70);
+            this.b2.TabIndex = 4;
+            this.b2.Text = "B2";
+            this.b2.UseVisualStyleBackColor = false;
+            this.b2.Click += new System.EventHandler(this.b2_Click);
+            // 
+            // b3
+            // 
+            this.b3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.b3.Font = new System.Drawing.Font("Segoe Script", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b3.Location = new System.Drawing.Point(152, 138);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(70, 70);
+            this.b3.TabIndex = 5;
+            this.b3.Text = "B3";
+            this.b3.UseVisualStyleBackColor = false;
+            this.b3.Click += new System.EventHandler(this.b3_Click);
+            // 
+            // c1
+            // 
+            this.c1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.c1.Font = new System.Drawing.Font("Segoe Script", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c1.Location = new System.Drawing.Point(0, 214);
+            this.c1.Name = "c1";
+            this.c1.Size = new System.Drawing.Size(70, 70);
+            this.c1.TabIndex = 6;
+            this.c1.Text = "C1";
+            this.c1.UseVisualStyleBackColor = false;
+            this.c1.Click += new System.EventHandler(this.c1_Click);
+            // 
+            // c2
+            // 
+            this.c2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.c2.Font = new System.Drawing.Font("Segoe Script", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c2.Location = new System.Drawing.Point(76, 214);
+            this.c2.Name = "c2";
+            this.c2.Size = new System.Drawing.Size(70, 70);
+            this.c2.TabIndex = 7;
+            this.c2.Text = "C2";
+            this.c2.UseVisualStyleBackColor = false;
+            this.c2.Click += new System.EventHandler(this.c2_Click);
+            // 
+            // c3
+            // 
+            this.c3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.c3.Font = new System.Drawing.Font("Segoe Script", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c3.Location = new System.Drawing.Point(152, 214);
+            this.c3.Name = "c3";
+            this.c3.Size = new System.Drawing.Size(70, 70);
+            this.c3.TabIndex = 8;
+            this.c3.Text = "C3";
+            this.c3.UseVisualStyleBackColor = false;
+            this.c3.Click += new System.EventHandler(this.c3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 40);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label1";
+            // 
+            // gamename2
+            // 
+            this.gamename2.AutoSize = true;
+            this.gamename2.Font = new System.Drawing.Font("Segoe Script", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gamename2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.gamename2.Location = new System.Drawing.Point(12, 9);
+            this.gamename2.Name = "gamename2";
+            this.gamename2.Size = new System.Drawing.Size(167, 40);
+            this.gamename2.TabIndex = 10;
+            this.gamename2.Text = "Tic Tac Toe";
+            this.gamename2.Click += new System.EventHandler(this.gamename2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe Script", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(227, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(173, 30);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Player O wins :";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe Script", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(228, 235);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(172, 30);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Player X wins :";
+            // 
+            // powins
+            // 
+            this.powins.AutoSize = true;
+            this.powins.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.powins.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.powins.Location = new System.Drawing.Point(406, 155);
+            this.powins.Name = "powins";
+            this.powins.Size = new System.Drawing.Size(24, 25);
+            this.powins.TabIndex = 14;
+            this.powins.Text = "0";
+            this.powins.Click += new System.EventHandler(this.powins_Click);
+            // 
+            // pxwins
+            // 
+            this.pxwins.AutoSize = true;
+            this.pxwins.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pxwins.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pxwins.Location = new System.Drawing.Point(406, 240);
+            this.pxwins.Name = "pxwins";
+            this.pxwins.Size = new System.Drawing.Size(24, 25);
+            this.pxwins.TabIndex = 15;
+            this.pxwins.Text = "0";
+            this.pxwins.Click += new System.EventHandler(this.pxwins_Click);
+            // 
+            // restart
+            // 
+            this.restart.Font = new System.Drawing.Font("Segoe Script", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.restart.Location = new System.Drawing.Point(43, 303);
+            this.restart.Name = "restart";
+            this.restart.Size = new System.Drawing.Size(136, 35);
+            this.restart.TabIndex = 16;
+            this.restart.Text = "Restart";
+            this.restart.UseVisualStyleBackColor = true;
+            this.restart.Click += new System.EventHandler(this.restart_Click);
+            // 
+            // gamewinner
+            // 
+            this.gamewinner.AutoSize = true;
+            this.gamewinner.Font = new System.Drawing.Font("Segoe Script", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gamewinner.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.gamewinner.Location = new System.Drawing.Point(227, 62);
+            this.gamewinner.Name = "gamewinner";
+            this.gamewinner.Size = new System.Drawing.Size(110, 30);
+            this.gamewinner.TabIndex = 17;
+            this.gamewinner.Text = "Winner:  ";
+            this.gamewinner.Click += new System.EventHandler(this.gamewinner_Click);
+            // 
+            // win
+            // 
+            this.win.AutoSize = true;
+            this.win.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.win.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.win.Location = new System.Drawing.Point(384, 67);
+            this.win.Name = "win";
+            this.win.Size = new System.Drawing.Size(18, 25);
+            this.win.TabIndex = 18;
+            this.win.Text = " ";
+            this.win.Click += new System.EventHandler(this.win_Click);
+            // 
+            // gamename
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(517, 350);
+            this.Controls.Add(this.win);
+            this.Controls.Add(this.gamewinner);
+            this.Controls.Add(this.restart);
+            this.Controls.Add(this.pxwins);
+            this.Controls.Add(this.powins);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.gamename2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.c3);
+            this.Controls.Add(this.c2);
+            this.Controls.Add(this.c1);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.a3);
+            this.Controls.Add(this.a2);
+            this.Controls.Add(this.a1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "gamename";
+            this.Text = "Tic Tac Toe by SJ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button a1;
+        private System.Windows.Forms.Button a2;
+        private System.Windows.Forms.Button a3;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button c1;
+        private System.Windows.Forms.Button c2;
+        private System.Windows.Forms.Button c3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label gamename2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label powins;
+        private System.Windows.Forms.Label pxwins;
+        private System.Windows.Forms.Button restart;
+        private System.Windows.Forms.Label gamewinner;
+        private System.Windows.Forms.Label win;
+    }
+}
+
---
+++ Documents/Visual Studio 2012/Projects/TicTacToe_GUI/TicTacToe_GUI/Form1.cs
@@ -0,0 +1,419 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe_GUI
+{
+    
+    public partial class gamename : Form
+    {
+        int count = 0; int pxwin = 0; int powin = 0;
+      
+        public gamename()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void gamename2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void a1_Click(object sender, EventArgs e)
+        {
+            if (a1.Text == "A1")
+            {
+
+
+                count++;
+                if (count % 2 == 0)
+                {
+                    a1.Text = "X";
+                }
+                else
+                    a1.Text = "O";
+                if (a1.Text == a2.Text && a2.Text == a3.Text || a1.Text == b1.Text && b1.Text == c1.Text || a1.Text == b2.Text && b2.Text == c3.Text)
+                {
+                    if (a1.Text == "X")
+                    {
+                        win.Text = " Player X";
+                        pxwin++;
+                        pxwins.Text = Convert.ToString(pxwin);
+                    }
+                    else
+                    {
+                        win.Text = "Player O";
+                        powin++;
+                        powins.Text = Convert.ToString(powin);
+                    }
+                }
+
+                if (count >= 8&& win.Text==" ")
+                {
+                    win.Text = " Draw ";
+
+
+                }
+            }
+           
+        }
+        private void a2_Click(object sender, EventArgs e)
+        {
+            if (a2.Text == "A2")
+            {
+
+
+                count++;
+                if (count % 2 == 0)
+                {
+                    a2.Text = "X";
+                }
+                else
+                    a2.Text = "O";
+                if (a1.Text == a2.Text && a2.Text == a3.Text || a2.Text == b2.Text && b2.Text == c2.Text)
+                {
+                    if (a2.Text == "X")
+                    {
+                        win.Text = " Player X";
+                        pxwin++;
+
+                        pxwins.Text = Convert.ToString(pxwin);
+                    }
+                    else
+                    {
+                        win.Text = "Player O";
+                        powin++;
+                        powins.Text = Convert.ToString(powin);
+                    }
+                }
+                if (count >= 8 && win.Text == " ")
+                {
+                    win.Text = " Draw ";
+
+
+                }
+            }
+           
+        }
+        private void a3_Click(object sender, EventArgs e)
+        {
+            if (a3.Text == "A3")
+            {
+                count++;
+                if (count % 2 == 0)
+                {
+                    a3.Text = "X";
+                }
+                else
+                    a3.Text = "O";
+                if (a1.Text == a2.Text && a2.Text == a3.Text || a3.Text == b3.Text && b3.Text == c3.Text || a3.Text == b2.Text && b2.Text == c1.Text)
+                {
+                    if (a3.Text == "X")
+                    {
+                        win.Text = " Player X";
+                        pxwin++;
+
+                        pxwins.Text = Convert.ToString(pxwin);
+                    }
+                    else
+                    {
+                        win.Text = "Player O";
+                        powin++;
+                        powins.Text = Convert.ToString(powin);
+                    }
+                }
+                if (count >= 8 && win.Text == " ")
+                {
+                    win.Text = " Draw ";
+
+
+                }
+            }
+           
+        }
+        private void b1_Click(object sender, EventArgs e)
+        {
+            if (b1.Text == "B1")
+            {
+                count++;
+                if (count % 2 == 0)
+                {
+                    b1.Text = "X";
+
+                }
+                else
+                    b1.Text = "O";
+                if (b1.Text == b2.Text && b2.Text == b3.Text || a1.Text == b1.Text && b1.Text == c1.Text)
+                {
+                    if (b1.Text == "X")
+                    {
+                        win.Text = " Player X";
+                        pxwin++;
+                        pxwins.Text = Convert.ToString(pxwin);
+                    }
+                    else
+                    {
+                        win.Text = "Player O";
+                        powin++;
+                        powins.Text = Convert.ToString(powin);
+                    }
+                }
+                if (count >= 8 && win.Text == " ")
+                {
+                    win.Text = " Draw ";
+
+
+                }
+            }
+           
+        }
+
+        private void b2_Click(object sender, EventArgs e)
+        {
+            if (b2.Text == "B2")
+            {
+
+                count++;
+                if (count % 2 == 0)
+                {
+                    b2.Text = "X";
+
+                }
+                else
+                    b2.Text = "O";
+                if (b1.Text == b2.Text && b2.Text == b3.Text || a2.Text == b2.Text && b2.Text == c2.Text || a1.Text == b2.Text && b2.Text == c3.Text || a3.Text == b2.Text && b2.Text == c1.Text)
+                {
+                    if (b2.Text == "X")
+                    {
+                        win.Text = " Player X";
+                        pxwin++;
+                        pxwins.Text = Convert.ToString(pxwin);
+                    }
+                    else
+                    {
+                        win.Text = "Player O";
+                        powin++;
+                        powins.Text = Convert.ToString(powin);
+                    }
+                }
+                if (count >= 8 && win.Text == " ")
+                {
+                    win.Text = " Draw ";
+
+
+                }
+            }
+          
+        }
+
+        private void b3_Click(object sender, EventArgs e)
+        {
+            if (b3.Text == "B3")
+            {
+
+                count++;
+                if (count % 2 == 0)
+                {
+                    b3.Text = "X";
+
+                }
+                else
+                    b3.Text = "O";
+                if (b1.Text == b2.Text && b2.Text == b3.Text || a3.Text == b3.Text && b3.Text == c3.Text)
+                {
+                    if (b3.Text == "X")
+                    {
+                        win.Text = " Player X";
+                        pxwin++;
+                        pxwins.Text = Convert.ToString(pxwin);
+                    }
+                    else
+                    {
+                        win.Text = "Player O";
+                        powin++;
+                        powins.Text = Convert.ToString(powin);
+                    }
+                }
+                if (count >= 8 && win.Text == " ")
+                {
+                    win.Text = " Draw ";
+
+
+                }
+            }
+           
+        }
+        private void c3_Click(object sender, EventArgs e)
+        {
+            if (c3.Text == "C3")
+            {
+
+                count++;
+                if (count % 2 == 0)
+                {
+                    c3.Text = "X";
+
+                }
+                else
+                    c3.Text = "O";
+
+                if (c1.Text == c2.Text && c2.Text == c3.Text || a3.Text == b3.Text && b3.Text == c3.Text || a1.Text == b2.Text && b2.Text == c3.Text)
+                {
+                    if (c3.Text == "X")
+                    {
+                        win.Text = " Player X";
+                        pxwin++;
+                        pxwins.Text = Convert.ToString(pxwin);
+                    }
+                    else
+                    {
+                        win.Text = "Player O";
+                        powin++;
+                        powins.Text = Convert.ToString(powin);
+                    }
+                }
+                if (count >= 8 && win.Text == " ")
+                {
+                    win.Text = " Draw ";
+
+
+                }
+            }
+           
+        }
+
+        private void c2_Click(object sender, EventArgs e)
+        {
+            if (c2.Text == "C2")
+            {
+
+                count++;
+                if (count % 2 == 0)
+                {
+                    c2.Text = "X";
+
+                }
+                else
+                    c2.Text = "O";
+
+                if (c1.Text == c2.Text && c2.Text == c3.Text || a2.Text == b2.Text && b2.Text == c2.Text)
+                {
+                    if (c2.Text == "X")
+                    {
+                        win.Text = " Player X";
+                        pxwin++;
+                        pxwins.Text = Convert.ToString(pxwin);
+                    }
+                    else
+                    {
+                        win.Text = "Player O";
+                        powin++;
+                        powins.Text = Convert.ToString(powin);
+                    }
+                }
+                if (count >= 8 && win.Text == " ")
+                {
+                    win.Text = " Draw ";
+
+
+                }
+            }
+           
+        }
+        private void c1_Click(object sender, EventArgs e)
+        {
+            if (c1.Text == "C1")
+            {
+
+                count++;
+                if (count % 2 == 0)
+                {
+                    c1.Text = "X";
+
+                }
+                else
+                {
+                    c1.Text = "O";
+                }
+
+                if (c1.Text == c2.Text && c2.Text == c3.Text || a1.Text == b1.Text && b1.Text == c1.Text || a3.Text == b2.Text && b2.Text == c1.Text)
+                {
+                    if (c1.Text == "X")
+                    {
+                        win.Text = " Player X";
+                        pxwin++;
+                        pxwins.Text = Convert.ToString(pxwin);
+                    }
+                    else
+                    {
+                        win.Text = "Player O";
+                        powin++;
+                        powins.Text = Convert.ToString(powin);
+                    }
+                }
+                if (count >= 8 && win.Text == " ")
+                {
+                    win.Text = " Draw ";
+
+
+                }
+            }
+        }
+       
+        private void powins_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pxwins_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void restart_Click(object sender, EventArgs e)
+        {
+            
+
+            a1.Text = "A1";
+            a2.Text = "A2";
+            a3.Text = "A3";
+            b1.Text = "B1"; 
+            b2.Text = "B2";
+            b3.Text = "B3";
+            c1.Text = "C1";
+            c2.Text = "C2";
+            c3.Text = "C3";
+            count = 0;
+            win.Text = " ";
+        }
+
+        private void win_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gamewinner_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+    }
+}
